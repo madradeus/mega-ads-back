b2c55345976637cfb182855d2bd8b6c866f18342
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-02 17:31:27 +0100

    Added error handling

---
+++ utils/errors.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export class ValidationError extends Error {
+}
+
+export const handleError = (err: Error, req: Request, res: Response, next: NextFunction): void => {
+
+    console.error(err);
+    res
+        .status(err instanceof ValidationError ? 400 : 500)
+        .json({
+            message: err instanceof ValidationError ? err.message : 'Error, please try again later'
+        })
+
+};
