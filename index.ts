ea4165c18a3f5dbacbbacaf14ec122febb35274b
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-07 19:48:53 +0100

    Added Error handling middleware to index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import 'express-async-errors';
 import cors from "cors";
+import { handleError } from "./utils/errors";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors({
 app.use(json({
     limit: "50kb"
 }));
-// app.use(handleError);
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => console.log('listening on http://localhost:3001/'));
