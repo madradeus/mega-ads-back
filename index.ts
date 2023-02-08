0764f47c478969bcdeb2ba69e6e828f836c5ccec
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-08 18:48:09 +0100

    Added rate limiter

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import 'express-async-errors';
 import cors from "cors";
 import { handleError } from "./utils/errors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(cors({
 }));
 app.use(json({
     limit: "50kb"
+}));
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }));
 
 app.use(handleError);
