c697b6d397142e6ea43a88f710f7c110c718facc
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-22 19:09:30 +0100

    Deleted duplicated mock method

--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,5 +1,4 @@
 import { AdRecord } from "../records/ad.record";
-import { pool } from "../db/db";
 import { AdEntity, SimpleAdEntity } from "../types";
 import { defaultAd } from "./ad-record.test";
 import { v4 as uuid } from "uuid";
@@ -10,9 +9,9 @@ beforeAll(() => {
     newAd = new AdRecord(defaultAd)
 });
 
-afterAll(async () => {
-    await pool.end();
-})
+// afterAll(async () => {
+//     await pool.end();
+// })
 
 jest
     .spyOn(AdRecord, 'getOne')
@@ -37,24 +36,6 @@ jest
     .mockImplementation(async (): Promise<string> => {
         return uuid()
     })
-
-jest
-    .spyOn(AdRecord, 'getOne')
-    .mockImplementation(async (id: string) => {
-
-        if ( id === 'bca' ) {
-            return null
-        }
-        return new AdRecord({
-            id,
-            name: 'testowa',
-            description: 'testowy opis',
-            price: 99.99,
-            url: 'test.test.pl',
-            lat: 49.1553514,
-            lon: 22.4681735,
-        })
-    });
 
 
 jest
@@ -111,13 +92,14 @@ jest
 test('AdRecords.getOne returns data from db for single record', async () => {
     const ad = await AdRecord.getOne('abc');
 
+
     expect(ad).toBeDefined();
     expect(ad instanceof AdRecord).toBeTruthy()
     expect(ad.id).toBe('abc')
     expect(ad.name).toBe('testowa');
     expect(ad.description).toBe('testowy opis');
     expect(ad.price).toEqual(99.99);
-    expect(ad.url).toEqual('test.test.pl');
+    expect(ad.url).toEqual('https://test.test.pl');
     expect(ad.lat).toEqual(49.1553514);
     expect(ad.lon).toEqual(22.4681735)
 });
