1c20424dd9483f958ab097763fc3122f6d270df2
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-22 20:05:19 +0100

    Added config example and moved DB connection into config file

--- db/db.ts
+++ db/db.ts
@@ -1,10 +1,11 @@
 import * as mysql from "mysql2/promise";
+import { config } from "../config/config";
 
 export const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'mega_ads',
-    user: 'root',
-    password: 'root',
+    host: config.host,
+    database: config.database,
+    user: config.user,
+    password: config.password,
     namedPlaceholders: true,
     port: 8889,
     decimalNumbers: true
